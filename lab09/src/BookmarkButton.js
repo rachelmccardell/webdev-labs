60dab5c7d96c16847549c13e3a2c8cb72d40e037
Rachel McCardell <rachelmccardell2022@u.northwestern.edu>
2022-03-16 11:32:51 -0500

    lab9

---
+++ lab09/src/BookmarkButton.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {getHeaders} from './utils';
+
+class BookmarkButton extends React.Component {  
+
+    constructor(props) {
+        super(props);
+        this.toggleBookmark = this.toggleBookmark.bind(this);
+        this.bookmark = this.bookmark.bind(this);
+        this.unbookmark = this.unbookmark.bind(this);
+        this.fetchPost = this.props.fetchPost.bind(this);
+    }
+ 
+    toggleBookmark(ev) {
+        if (this.props.bookmarkId) {
+            console.log('unbookmark');
+            this.unbookmark();
+        } else {
+            console.log('bookmark');
+            this.bookmark();
+        }
+    }
+
+    bookmark() {
+        fetch(`/api/posts/${this.props.postId}/bookmarks`, {
+            headers: getHeaders(),
+            method: 'POST',
+            body: JSON.stringify({post_id: this.props.postId})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.fetchPost();
+        })
+    }
+
+    unbookmark() {
+       fetch(`api/posts/${this.props.postId}/bookmarks/${this.props.bookmarkId}`, {
+           headers: getHeaders(),
+           method: 'DELETE'
+       })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.fetchPost();
+       })
+    }
+
+    render () {
+        const bookmarkId = this.props.bookmarkId;
+        return (
+            <button role="switch"
+                className="bookmark" 
+                aria-label="Bookmark Button" 
+                aria-checked={bookmarkId ? true : false}
+                onClick={this.toggleBookmark}>
+                <i className={bookmarkId ? 'fas fa-bookmark' : 'far fa-bookmark'}></i>                        
+            </button>
+        ) 
+    }
+}
+
+export default BookmarkButton;
---
+++ lab09/src/LikeButton.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {getHeaders} from './utils';
+
+class LikeButton extends React.Component {  
+
+    constructor(props) {
+        super(props);
+        this.toggleLike = this.toggleLike.bind(this);
+        this.like = this.like.bind(this);
+        this.unlike = this.unlike.bind(this);
+        this.fetchPost = this.props.fetchPost.bind(this);
+    }
+ 
+    toggleLike(ev) {
+        if (this.props.likeId) {
+            console.log('unlike');
+            this.unlike();
+        } else {
+            console.log('like');
+            this.like();
+        }
+    }
+
+    like() {
+        fetch(`/api/posts/${this.props.postId}/likes`, {
+            headers: getHeaders(),
+            method: 'POST',
+            body: JSON.stringify({post_id: this.props.postId})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.fetchPost();
+        })
+    }
+
+    unlike() {
+       fetch(`api/posts/${this.props.postId}/likes/${this.props.likeId}`, {
+           headers: getHeaders(),
+           method: 'DELETE'
+       })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.fetchPost();
+       })
+    }
+
+    render () {
+        const likeId = this.props.likeId;
+        return (
+            <button role="switch"
+                className="like" 
+                aria-label="Like Button" 
+                aria-checked={likeId ? true : false}
+                onClick={this.toggleLike}>
+                <i className={likeId ? 'fas fa-heart' : 'far fa-heart'}></i>                        
+            </button>
+        ) 
+    }
+}
+
+export default LikeButton;
